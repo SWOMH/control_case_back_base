8b7d9d17d34ee1514e60ed8fac230c8872c3b3ff
SWOMH <tifame67@mail.ru>
2025-08-06 19:15:36 +0300

    upd models

--- database/models/balance.py
+++ database/models/balance.py
@@ -6,7 +6,16 @@ from database.base import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database.types import intpk
 
+class TypeDeduction(Base):
+    __tablename__ = "type_deduction"
+    id: Mapped[intpk]
+    type_name: Mapped[str]
 
+class TypeReplenishment(Base):
+    __tablename__ = "type_replenishment"
+    id: Mapped[intpk]
+    type_name: Mapped[str]
+    
 
 class HistoryBalanceDeductions(Base):
     __tablename__ = "history_deductions"
@@ -14,12 +23,19 @@ class HistoryBalanceDeductions(Base):
     period: Mapped[datetime] = mapped_column(DateTime, nullable=False) 
     value: Mapped[Decimal] = mapped_column(Numeric(10,2), default=0)    
     user_id: Mapped[int] = mapped_column(ForeignKey('public.users.id', ondelete='CASCADE'), nullable=False)
+    type: Mapped[int] = mapped_column(ForeignKey('public.type_deduction.id'))
+
+
     user = relationship("Users", back_populates="history_balance")
 
-class HistoryBalance(Base): # пополнение
-    __tablename__ = "history_bonuses"
+
+class HistoryBalanceReplenishment(Base): # пополнение
+    __tablename__ = "history_replenishment"
     id: Mapped[intpk]
     period: Mapped[datetime] = mapped_column(DateTime, nullable=False) 
     value: Mapped[Decimal] = mapped_column(Numeric(10,2), default=0)    
     user_id: Mapped[int] = mapped_column(ForeignKey('public.users.id', ondelete='CASCADE'), nullable=False)
+    type: Mapped[int] = mapped_column(ForeignKey('public.type_replenishment.id'))
+
+    
     user = relationship("Users", back_populates="history_balance")
