253662a76c0fe2dd32d124ca13f1995fb3f4df83
SWOMH <tifame67@mail.ru>
2025-08-17 18:05:43 +0300

    upd import

--- database/main_connection.py
+++ database/main_connection.py
@@ -1,8 +1,8 @@
 from database.core import DatabaseCore
-from config.constant import CONSTANT
+from constants import DEV_CONSTANT
 
 
 class DataBaseMainConnect(DatabaseCore):
     def __init__(self):
-        url_con = CONSTANT.url_connection
+        url_con = DEV_CONSTANT.url_connection
         super().__init__(str(url_con), create_tables=True)
