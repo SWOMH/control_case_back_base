bf700ac3a112a55c7a1cb4de95c387a3da9a38e0
SWOMH <tifame67@mail.ru>
2025-08-21 16:36:19 +0300

    upd models

--- database/models/documents.py
+++ database/models/documents.py
@@ -6,7 +6,11 @@ from database.base import Base
 from sqlalchemy.orm import Mapped, mapped_column
 from database.types import intpk
 
+
 class DocumentsUser(Base):
     __tablename__ = "documents_user"
     id: Mapped[intpk]
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("public.users.id"))
+    document_id: Mapped[int] = mapped_column(Integer, ForeignKey("public.documents.id"))
+    created_count: Mapped[int]
+    created_limit: Mapped[int | None]
--- database/models/news_feed.py
+++ database/models/news_feed.py
@@ -1,17 +1,27 @@
 from database.base import Base
 from decimal import Decimal
-from datetime import date
+from datetime import datetime
 from sqlalchemy import Date, DateTime, ForeignKey, Numeric, Text, text
 from sqlalchemy.orm import Mapped, mapped_column
-from database.types import intpk
+from database.types import intpk, u_id
 
 
 class Posts(Base):
     __tablename__ = "posts"
     id: Mapped[intpk]
+    time_created: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.utcnow, nullable=False)
     title: Mapped[str]
     img: Mapped[str | None]
     video: Mapped[str | None]
+    moderation: Mapped[bool]
+    author: Mapped[u_id]
+
+
+class Likes(Base):
+    __tablename__ = 'likes'
+    id: Mapped[intpk]
+    user_id: Mapped[u_id]
+    post_id: Mapped[int] = mapped_column(ForeignKey('public.posts.id'))
 
 
 class Comments(Base):
@@ -21,6 +31,8 @@ class Comments(Base):
     post_id: Mapped[int] = mapped_column(ForeignKey('public.posts.id', ondelete='CASCADE'), nullable=False)
     text: Mapped[str | None]
     image: Mapped[str | None]
+    deleted: Mapped[bool] = mapped_column(default=False)
+
 
 class CommentAnswer(Base):
     __tablename__ = "comments_answer"
