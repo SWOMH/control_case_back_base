9f6cbd56212e1f46c2acb68da83319332ff004a1
SWOMH <tifame67@mail.ru>
2025-09-08 00:04:28 +0300

    add settings config

---
+++ config/settings.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+
+class TypeUpdSchedule(str, Enum):
+    EACH_PAYMENT = 'each payment'  # С каждого платежа убирать
+    ONE_PAYMENT = 'one payment'  # С одного платежа
+
+
+class AppSettings(BaseModel):
+    automatic_upd_schedule: bool = Field(description='Автоматически обновлять расписание или это будет делать только '
+                                                     'оператор. (True - автоматически',
+                                         default=True)
+    type_upd_schedule: TypeUpdSchedule = Field(description='В случае переплаты по платежу как действовать в '
+                                                           'автоматическом режиме. Сверх снять с каждого платежа в '
+                                                           'графике или только с последнего?',
+                                               default=TypeUpdSchedule.EACH_PAYMENT)
