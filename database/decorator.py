9adc72091ea3730c264dc14ef121757ae75ae347
SWOMH <tifame67@mail.ru>
2025-09-16 23:57:19 +0300

    fix deco

--- database/decorator.py
+++ database/decorator.py
@@ -4,44 +4,20 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import text
 
 
-def connection(self, isolation_level: Optional[str] = None, commit: bool = True):
-    """
-    Декоратор для управления сессией с возможностью настройки уровня изоляции и коммита.
-    Теперь можно выбирать подходящий уровень изоляции для разных операций:
-
-    READ COMMITTED — для обычных запросов (по умолчанию в PostgreSQL).
-    SERIALIZABLE — для финансовых операций, требующих максимальной надежности.
-    REPEATABLE READ — для отчетов и аналитики.
-
-    Параметры:
-    - `isolation_level`: уровень изоляции для транзакции (например, "SERIALIZABLE").
-    - `commit`: если `True`, выполняется коммит после вызова метода.
-
-    # Чтение данных (на самом деле нах не надо, ибо по умолчанию так и проходит)
-    @connection(isolation_level="READ COMMITTED")
-    async def get_user(self, session, user_id: int):
-        ...
-
-    # Финансовая операция
-    @connection(isolation_level="SERIALIZABLE", commit=False)
-    async def transfer_money(self, session, from_id: int, to_id: int):
-        ...
-    """
-    def decorator(method):
+def connection(isolation_level: Optional[str] = None, commit: bool = True):
+    def decorator(method: Coroutine):
         @wraps(method)
-        async def wrapper(*args, **kwargs):
-            async with self.session_maker() as session:
+        async def wrapper(self, *args, **kwargs):
+            async with self.Session() as session:
                 try:
                     if isolation_level:
                         await session.execute(text(f"SET TRANSACTION ISOLATION LEVEL {isolation_level}"))
-                    result = await method(*args, session=session, **kwargs)
+                    result = await method(self, session=session, *args, **kwargs)
                     if commit:
                         await session.commit()
                     return result
-                except Exception as e:
+                except Exception:
                     await session.rollback()
-                    raise e
-                finally:
-                    await session.close()
+                    raise
         return wrapper
     return decorator
